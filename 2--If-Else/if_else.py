d411f0a2c700f6f49cabd265bfa53a85383447d4
mechaphantom <spikecowboy123@gmail.com>
2022-08-03 23:57:11 +0300

    adding new solution way for solution #2

--- 2--If-Else/if_else.py
+++ 2--If-Else/if_else.py
@@ -1,3 +1,4 @@
+#Solution Way #1 for Python3
 n = int(input())
 if (n <= 100 and n >= 1):
     if (n % 2 != 0):
@@ -9,3 +10,17 @@ if (n <= 100 and n >= 1):
             print("Weird")
         if (n > 20):
             print("Not Weird")
+
+#Solution Way #2 for Python2
+"""
+import sys
+n = int(raw_input().strip())
+if n%2==1:
+    print "Weird"    
+elif n%2 == 0 and 2 <= n <= 5:
+    print "Not Weird"
+elif n%2 == 0 and 6 <= n <= 20:
+    print "Weird"
+else:
+    print "Not Weird"
+"""
