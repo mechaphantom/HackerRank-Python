9dad6ef957042d4a377b4c16b89603da5e3ccabf
mechaphantom <spikecowboy123@gmail.com>
2022-08-06 23:47:14 +0300

    adding new solution way for 7th problem

--- 7--Print-Function/print-function.py
+++ 7--Print-Function/print-function.py
@@ -4,3 +4,9 @@ if(n <= 150 and n >= 1):
     while(i <= n):
         print(i, sep='', end='')
         i += 1
+
+#Solution Way #2 for Python2
+"""
+from __future__ import print_function
+print(*[i for i in range(1, int(raw_input())+1)], sep='')
+"""
