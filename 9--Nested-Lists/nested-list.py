96c74eaa9917c43a932d922dda32f5bf104f7dbb
mechaphantom <spikecowboy123@gmail.com>
2022-08-10 22:33:12 +0300

    solution for 9th problem

---
+++ 9--Nested-Lists/nested-list.py
@@ -0,0 +1,15 @@
+#Solution for Python 2
+d={} 
+for _ in range(int(raw_input())): 
+    Name=raw_input() 
+    Grade=float(raw_input()) 
+    d[Name]=Grade 
+v=d.values()
+second=sorted(list(set(v)))[1] 
+second_lowest=[] 
+for key,value in d.items():  
+    if value==second: 
+        second_lowest.append(key) 
+second_lowest.sort() 
+for name in second_lowest: 
+    print(name)
