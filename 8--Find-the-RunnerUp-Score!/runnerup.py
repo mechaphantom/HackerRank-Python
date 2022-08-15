0ce9f0777357c6569eca42884a9749c00183060e
mechaphantom <spikecowboy123@gmail.com>
2022-08-15 22:38:11 +0300

    adding new solution way for 8th problem

--- 8--Find-the-RunnerUp-Score!/runnerup.py
+++ 8--Find-the-RunnerUp-Score!/runnerup.py
@@ -6,3 +6,25 @@ if __name__ == '__main__':
         list2 = list(set(list1))
         list2.sort()
         print(list2[-2])
+
+# Another possible solution
+"""
+def findSM(l):
+    f, s = l[0], l[0]
+    for i in range(len(l)):
+        if l[i] > f:
+            s, f = f, l[i]
+        elif l[i] < f:
+            if f == s:
+                s = l[i]
+            elif l[i] > s:
+                s = l[i]
+    return s            
+
+n = int(input())
+l = input().split()
+for i in range(n):
+    l[i] = int(l[i])
+
+print(findSM(l))
+"""
