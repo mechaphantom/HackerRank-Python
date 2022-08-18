b3b854678d81b072800b29fd0f16fb0a3a527421
mechaphantom <spikecowboy123@gmail.com>
2022-08-18 22:57:02 +0300

    adding new solution way for 11th problem

--- 11--Tuples/tuples.py
+++ 11--Tuples/tuples.py
@@ -3,3 +3,10 @@ if __name__ == '__main__':
     integer_list = map(int, input().split())
     t = tuple(integer_list)
     print(hash(t))
+
+#Another possible solution for Python 3
+"""
+N = int(input())
+T = tuple(int(x) for x in input().split())
+print(hash(T))
+"""
