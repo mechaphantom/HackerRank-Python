d1e78f576769e8971d0cc705242778ae4a6f5552
mechaphantom <spikecowboy123@gmail.com>
2022-08-27 15:45:46 +0300

    adding new solution way for 25th problem

--- 25--No-Idea!/no_idea.py
+++ 25--No-Idea!/no_idea.py
@@ -9,3 +9,24 @@ for i in l:
     if i in secondset:
         p-=1
 print(p)
+
+# Another Possible Solution Way
+"""
+n, m = input().split()
+n_int, m_int = int(n), int(m)
+n_integers_for_array = input().split()
+n_array = [eval(i) for i in n_integers_for_array]
+m1_integers_for_set, m2_integers_for_set = input().split(), input().split()
+m1, m2 = list(set(m1_integers_for_set)), list(set(m2_integers_for_set))
+if(((n_int <= 10**9) and (n_int >= 1)) and ((m_int <= 10**9) and (m_int >= 1)) and (x for x in m1 if m1[x] <= 10**9 and m1[x] >= 1) and (x for x in m2 if m2[x] <= 10**9 and m2[x] >= 1)):
+    if(((n_int <= 10**5) and (n_int >= 1)) and ((m_int <= 10**5) and (m_int >= 1))):
+        happiness = 0
+        for i in range(len(m1)):
+            if(m1[i] in str(n_int)):
+                happiness += 1
+                print("+ ", happiness)
+            if(m2[i] in str(n_int)):
+                happiness -= 1
+                print("- ", happiness)
+        print(happiness)
+"""
